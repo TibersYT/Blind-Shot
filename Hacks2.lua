3607716b8a1469190930fd9df89a376e8ebb0f9e
Unknownproooolucky <chmoraum@gmail.com>
2025-12-15 17:19:07 +1000

    Add loop to make GUI abilities visible

---
+++ Hacks2.lua
@@ -0,0 +1,6 @@
+while wait() do
+game.Players.LocalPlayer.PlayerGui.ScreenGui.habilidades.nuke.Visible = True
+game.Players.LocalPlayer.PlayerGui.ScreenGui.habilidades.forcefield.Visible = True
+game.Players.LocalPlayer.PlayerGui.ScreenGui.habilidades.reveal.Visible = True
+game.Players.LocalPlayer.PlayerGui.ScreenGui.habilidades.bomb.Visible = True
+end
